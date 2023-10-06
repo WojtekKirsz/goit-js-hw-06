4d1714f5cf25e69201520e2518773ee38acf3f32
WojtekKirsz <w.kirsz@gmail.com>
2023-10-06 18:25:26 +0200

    Task #1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+//Task # 1
+
+const listOfCategories = document.querySelectorAll("li.item");
+const numberOfCategories = listOfCategories.length;
+console.log(`Number of categories: ${numberOfCategories}`);
+
+listOfCategories.forEach((listOfCategory) => {
+  const nameHeader = listOfCategory.querySelector("h2");
+  const elements = listOfCategory.querySelectorAll("ul li");
+  const numberOfElements = elements.length;
+
+  console.log(`Category: ${nameHeader.textContent}`);
+  console.log(`Elements: ${numberOfElements}`);
+});
