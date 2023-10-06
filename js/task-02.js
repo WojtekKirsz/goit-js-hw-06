3e1f3c8a64fbb095db74b34afddd441a9bcd0032
WojtekKirsz <w.kirsz@gmail.com>
2023-10-06 19:06:25 +0200

    Task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredients = document.getElementById("ingredients");
+
+ingredients.forEach((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item");
+  listOfIngredients.append(ingredientItem);
+});
