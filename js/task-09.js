be7a13afd4c7f93c303e441d0114714c6e891368
WojtekKirsz <w.kirsz@gmail.com>
2023-10-09 20:37:24 +0200

    completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const selectedBodyColor = document.querySelector("body");
+
+const selectedColor = document.querySelector(".color");
+console.log(selectedColor);
+
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const randomHexColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
+
+  selectedColor.textContent = randomHexColor;
+  selectedBodyColor.style.backgroundColor = randomHexColor;
 }
