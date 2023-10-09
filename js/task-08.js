283d0049729e338b413dffce855cc6ff509cbf89
WojtekKirsz <w.kirsz@gmail.com>
2023-10-09 18:12:26 +0200

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
+console.log(form);
 
+form.addEventListener("submit", runForm);
+
+function runForm(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return console.log("Please fill all the required fields");
+  } else {
+    const credentials = {
+      email: email,
+      password: password,
+    };
+    console.log(credentials);
+  }
+  form.reset();
+}
